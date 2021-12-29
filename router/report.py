f5c451fff02b03f737e601ff82b3caa4e867520e
Universe1122 <whdals7979@gmail.com>
2021-12-29 05:55:01 +0000

    malwares api 구현 완료

--- router/report.py
+++ router/report.py
@@ -5,6 +5,7 @@ import validators
 from feature.virustotal import Virustotal
 from feature.google_safe_browsing import GoogleSafeBrowsing
 from feature.phishtank import Phishtank
+from feature.malwares import Malwares
 
 
 api_report = Namespace("API")
@@ -26,12 +27,14 @@ class ApiReport(Resource):
                 virustotal_reuslt = Virustotal().start(args["url"])
                 google_safe_browsing_result = GoogleSafeBrowsing().start(args["url"])
                 phishtank_result = Phishtank().start(args["url"])
+                malwares_result = Malwares().start(args["url"])
 
                 return {
                     "result" : {
                         "virustotal" : virustotal_reuslt,
                         "google_safe_browsing" : google_safe_browsing_result,
-                        "phishtank" : phishtank_result
+                        "phishtank" : phishtank_result,
+                        "malwares" : malwares_result
                     }
                 }, 200
 
