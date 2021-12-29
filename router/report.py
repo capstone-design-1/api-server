6ae4984b7be10181e5da17ae0e4c68936d15c72d
Universe1122 <whdals7979@gmail.com>
2021-12-29 04:26:21 +0000

    url 검증 코드 추가

--- router/report.py
+++ router/report.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Resource, Api, Namespace, reqparse
+import validators
 
 from feature.virustotal import Virustotal
 
@@ -15,6 +16,21 @@ class ApiReport(Resource):
 
     def get(self):
         args = parser.parse_args()
-        result = Virustotal().start(args["url"])
 
-        return {"result" : result}
+        try:
+            validate_url = validators.url(args["url"])
+
+            if validate_url == True:
+                result = Virustotal().start(args["url"])
+                return {"result" : result}, 200
+            else:
+                return {
+                    "result" : "error",
+                    "message" : "유효하지 않은 URL 입니다."
+                }, 400
+
+        except TypeError:
+            return {
+                "result" : "error",
+                "message" : "URL 파라미터가 존재하지 않습니다."
+            }, 400
